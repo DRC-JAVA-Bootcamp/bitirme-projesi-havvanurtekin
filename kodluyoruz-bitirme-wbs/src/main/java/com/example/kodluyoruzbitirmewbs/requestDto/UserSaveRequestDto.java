fb86d9c19a0e80adc2b3f0b9f658a4883b68c232
Havva Nur TEKİN <20120205304@ismu.edu.tr>
2022-05-05 00:10:27 +0300

    Team
    Tüm classlar için Controller/Entity/Dto/Reporsitory ve Service katmanları oluşturuldu save metodları oluşturuldu.

--- kodluyoruz-bitirme-wbs/src/main/java/com/example/kodluyoruzbitirmewbs/requestDto/UserSaveRequestDto.java
+++ kodluyoruz-bitirme-wbs/src/main/java/com/example/kodluyoruzbitirmewbs/requestDto/UserSaveRequestDto.java
@@ -4,10 +4,6 @@ import com.example.kodluyoruzbitirmewbs.entity.Team;
 import com.example.kodluyoruzbitirmewbs.entity.UserRole;
 import lombok.Data;
 
-import javax.persistence.Column;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-
 @Data
 public class UserSaveRequestDto {
     private String name;
